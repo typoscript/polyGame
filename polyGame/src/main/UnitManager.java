ca926f307d4cebe8c7a95c423f3b731cedeae5db
typoscript <minkyu.kim.contact@gmail.com>
2024-04-01 16:31:39 +0900

    Add Member Variables

--- polyGame/src/main/UnitManager.java
+++ polyGame/src/main/UnitManager.java
@@ -1,5 +1,10 @@
 package main;
 
-public class UnitManager {
+import java.util.ArrayList;
 
+public class UnitManager {
+	ArrayList<Unit> players = new ArrayList<>();
+	ArrayList<Unit> monsters = new ArrayList<>();
+	private final String PATH = "main.";
+	private final String[] monsterClassNames = { "Mushroom", "Ghost", "Orc" };
 }
