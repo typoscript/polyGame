28a901509ccdbd56525783660c170652aa3be37d
typoscript <minkyu.kim.contact@gmail.com>
2024-04-01 16:09:46 +0900

    Change Warrior Stats

--- polyGame/src/main/Warrior.java
+++ polyGame/src/main/Warrior.java
@@ -1,14 +1,14 @@
 package main;
 
 public class Warrior extends Unit {
-	private final int INIT_ATTACK_POWER = 5;
-	private final int INIT_MAX_HP = 5;
+	private final int INIT_ATTACK_POWER = 30;
+	private final int INIT_MAX_HP = 200;
 
 	public Warrior() {
 		this.name = "전사";
-		this.attackPower = INIT_ATTACK_POWER;
 		this.hp = INIT_MAX_HP;
 		this.maxHp = INIT_MAX_HP;
+		this.attackPower = INIT_ATTACK_POWER;
 	}
 	
 	public void attack(Unit target) {
