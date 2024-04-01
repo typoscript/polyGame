16102b120215f0eec8a64c09783b02af69dc90c6
typoscript <minkyu.kim.contact@gmail.com>
2024-04-01 18:30:08 +0900

    Modify Method: chooseAction()

--- polyGame/src/hero/Mage.java
+++ polyGame/src/hero/Mage.java
@@ -36,6 +36,7 @@ public class Mage extends Unit implements Actionable {
 		
 		while (true) {
 			System.out.printf("[%s] 1) 공격 2) 공격력 증가 스킬\n", name);
+			System.out.print("메뉴: ");
 			
 			int action = GameManager.sc.nextInt();
 			
