11117963935481ce42611930e912b15e8ad5ef02
typoscript <minkyu.kim.contact@gmail.com>
2024-04-01 18:17:01 +0900

    Modify Method: run()

--- polyGame/src/main/GameManager.java
+++ polyGame/src/main/GameManager.java
@@ -1,5 +1,45 @@
 package main;
 
-public class GameManager {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+import stage.Stage;
+import stage.StageBattle;
+import stage.StageLobby;
+import stage.StageName;
+
+public class GameManager {
+	public static Scanner sc = new Scanner(System.in);
+	public static boolean isRunning = true;
+	private int stageLevel = 1;
+	private Map<StageName, Stage> stages = new HashMap<StageName, Stage>();
+	private StageName currentStage;
+	
+	public GameManager() {
+		stages.put(StageName.LOBBY, new StageLobby());
+		stages.put(StageName.BATTLE, new StageBattle());
+		
+		currentStage = StageName.LOBBY;
+	}
+
+	public void run() {
+		while (isRunning) {
+			Stage stage = stages.get(currentStage);
+			
+			stage.run();
+			
+			switch (currentStage) {
+				case LOBBY:
+					currentStage = StageName.BATTLE;
+					break;
+				case BATTLE:
+					currentStage = StageName.LOBBY;
+					stageLevel++;
+					break;
+			}
+		}
+		
+		System.out.println("게임 종료");
+	}
 }
