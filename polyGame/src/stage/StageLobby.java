7fd0c26a5b81c4b1a41901f388e7de4bc30c2e8b
typoscript <minkyu.kim.contact@gmail.com>
2024-04-01 17:48:50 +0900

    Modify Method: run()

--- polyGame/src/stage/StageLobby.java
+++ polyGame/src/stage/StageLobby.java
@@ -1,6 +1,11 @@
 package stage;
 
+import main.GameManager;
+
 public class StageLobby extends Stage {
+	private final int MENU_BATTLE = 1;
+	private final int MENU_QUIT = 2;
+
 	@Override
 	public void init() {
 	}
@@ -10,5 +15,18 @@ public class StageLobby extends Stage {
 		System.out.println("=== 로비 ====");
 		System.out.println("1) 전투 2) 종료");
 		System.out.println("===========");
+		
+		while (true) {
+			System.out.print("메뉴: ");
+			int menu = GameManager.sc.nextInt();
+			
+			switch (menu) {
+				case MENU_BATTLE:
+					return;
+				case MENU_QUIT:
+					GameManager.isRunning = false;
+					return;
+			}
+		}
 	}
 }
