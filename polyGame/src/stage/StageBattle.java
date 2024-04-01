5861bb116ab65c3af4905876e22493911a0df508
typoscript <minkyu.kim.contact@gmail.com>
2024-04-01 17:54:57 +0900

    Add File: StageBattle.java

---
+++ polyGame/src/stage/StageBattle.java
@@ -0,0 +1,13 @@
+package stage;
+
+public class StageBattle extends Stage {
+	@Override
+	public void init() {
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
