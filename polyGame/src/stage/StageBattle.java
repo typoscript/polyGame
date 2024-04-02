a7526319ab2d7ab0d8da54309fa354ee541682f0
typoscript <minkyu.kim.contact@gmail.com>
2024-04-02 10:02:29 +0900

    Add Member Variables

--- polyGame/src/stage/StageBattle.java
+++ polyGame/src/stage/StageBattle.java
@@ -1,13 +1,37 @@
 package stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import hero.Actionable;
+import hero.Healer;
+import main.GameManager;
+import main.Unit;
+import main.UnitManager;
+
 public class StageBattle extends Stage {
+	UnitManager unitManager = new UnitManager();
+	List<Unit> heroes = new ArrayList<Unit>();
+	List<Unit> monsters = new ArrayList<Unit>();
+	
 	@Override
 	public void init() {
+		heroes = unitManager.getHeroes();
+		monsters = unitManager.getMonsters();
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		init();
 		
+		while (isRunning()) {
+			printUnitAll();
+			
+			heroesTakeTurn();
+			monstersAttackHeroes();
+		}
+		
+		if (heroes.size() == 0)
+			GameManager.isRunning = false;
 	}
 }
