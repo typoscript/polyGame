b2f350da36972dcae522425845566322c5a9c370
typoscript <minkyu.kim.contact@gmail.com>
2024-04-02 12:31:06 +0900

    Refactor Method: Constructor, Remove Memeber Variables

--- polyGame/src/monsters/Mushroom.java
+++ polyGame/src/monsters/Mushroom.java
@@ -3,14 +3,11 @@ package monsters;
 import main.Unit;
 
 public class Mushroom extends Unit {
-	private final int INIT_ATTACK_POWER = 10;
-	private final int INIT_MAX_HP = 400;
-
 	public Mushroom() {
-		this.name = "버섯";
-		this.hp = INIT_MAX_HP;
-		this.maxHp = INIT_MAX_HP;
-		this.attackPower = INIT_ATTACK_POWER;
+		name = "버섯";
+		maxHp = 400;
+		hp = maxHp;
+		attackPower = 10;
 	}
 	
 	@Override
