c8d6257e363531525aab5f538df5ca266e1a70e7
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 11:50:08 +0900

    Add Member Variables

--- polyGame/src/main/FileManager.java
+++ polyGame/src/main/FileManager.java
@@ -1,5 +1,11 @@
 package main;
 
-public class FileManager {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 
+public class FileManager {
+	private FileReader fr;
+	private FileWriter fw;
+	private BufferedReader br;
 }
