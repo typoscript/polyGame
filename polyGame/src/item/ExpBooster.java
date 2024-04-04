e45952138018ff2bba7319392355b7df9533d015
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 10:30:58 +0900

    Add Unimplemented Methods

--- polyGame/src/item/ExpBooster.java
+++ polyGame/src/item/ExpBooster.java
@@ -1,5 +1,14 @@
 package item;
 
-public class ExpBooster extends Item implements ItemUsable {
+import main.Unit;
 
+public class ExpBooster extends Item implements ItemUsable {
+	public ExpBooster() {
+		super("경험치 부스터", 5000);
+	}
+
+	@Override
+	public void use(Unit target) {
+		target.increaseExpGain();
+	}
 }
