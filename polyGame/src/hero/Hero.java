cfed1c856c2f14ee6c52b717c27e4d749e36a1fb
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 11:03:34 +0900

    Add Member Variables

--- polyGame/src/hero/Hero.java
+++ polyGame/src/hero/Hero.java
@@ -1,12 +1,19 @@
 package hero;
 
+import item.Armor;
+import item.ItemUsable;
+import item.Weapon;
 import main.Unit;
 
 public abstract class Hero extends Unit {
 	protected int level = 1;
 	protected int exp = 0;
 	protected int expMax = 100;
-	protected int armor = 0;
+
+	protected boolean hasParty;
+	protected Weapon weapon;
+	protected Armor armor;
+	protected ItemUsable[] usableItems;
 	
 	abstract public void attack(Unit target);
 }
