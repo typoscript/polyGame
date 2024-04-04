146cbf4f05b6fa57d3da394d64960863b23c2ac7
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 11:23:58 +0900

    Add File: StageMarket.java

---
+++ polyGame/src/stage/StageMarket.java
@@ -0,0 +1,15 @@
+package stage;
+
+public class StageMarket extends Stage {
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public StageName run() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
