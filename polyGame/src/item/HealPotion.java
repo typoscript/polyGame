c0ecc569934891ec5fc141cf96d2a0b70243e634
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 10:25:33 +0900

    Add Unimplemented Methods

--- polyGame/src/item/HealPotion.java
+++ polyGame/src/item/HealPotion.java
@@ -4,4 +4,13 @@ import main.Unit;
 
 public class HealPotion extends Item implements ItemUsable {
 	public final int healAmount = 30;
+
+	public HealPotion() {
+		super("회복약", 1000);
+	}
+
+	@Override
+	public void use(Unit target) {
+		target.increaseHp(healAmount);
+	}
 }
