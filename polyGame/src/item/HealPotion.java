80c20186536175df203b3f555dc6afd7d82749bc
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 15:58:12 +0900

    Add Method: toString()

--- polyGame/src/item/HealPotion.java
+++ polyGame/src/item/HealPotion.java
@@ -3,7 +3,7 @@ package item;
 import main.Unit;
 
 public class HealPotion extends Item implements Usable {
-	public final int healAmount = 30;
+	public final int HEAL_AMOUNT = 30;
 
 	public HealPotion() {
 		super("회복약", 1000);
@@ -11,6 +11,11 @@ public class HealPotion extends Item implements Usable {
 
 	@Override
 	public void use(Unit target) {
-		target.increaseHp(healAmount);
+		target.increaseHp(HEAL_AMOUNT);
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("이름: %s | 가격: %d | 회복력: %d", NAME, PRICE, HEAL_AMOUNT);
 	}
 }
