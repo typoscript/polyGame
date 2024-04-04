8a12547f892889685d46f60bff09d36a645d7eb8
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 10:51:36 +0900

    Change Weapon Class -> Inteface

--- polyGame/src/item/Weapon.java
+++ polyGame/src/item/Weapon.java
@@ -1,14 +1,5 @@
 package item;
 
-import main.Unit;
-
-public class WeaponUpgrader extends Item implements ItemUsable {
-	public WeaponUpgrader() {
-		super("무기 업그레이드", 3000);
-	}
-
-	@Override
-	public void use(Unit target) {
-		target.upgradeWeapon(10);
-	}
+public interface Weapon {
+	
 }
