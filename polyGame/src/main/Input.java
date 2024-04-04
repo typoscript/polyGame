8b9a6d7802f97f96bc4b1224f9ec4b9b7a31ea00
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 15:09:10 +0900

    Add Method: getInputString()

--- polyGame/src/main/Input.java
+++ polyGame/src/main/Input.java
@@ -4,4 +4,9 @@ import java.util.Scanner;
 
 public class Input {
 	public static Scanner sc = new Scanner(System.in);
+	
+	public static String getInputString(String message) {
+		System.out.print(message + ": ");
+		return sc.next();
+	}
 }
