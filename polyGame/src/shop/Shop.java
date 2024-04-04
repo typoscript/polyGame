c449a2de220f415624928adc74961a437985ca60
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 16:31:17 +0900

    Modify Method: runBuyItem()

--- polyGame/src/shop/Shop.java
+++ polyGame/src/shop/Shop.java
@@ -3,6 +3,7 @@ package shop;
 import java.util.ArrayList;
 import java.util.List;
 
+import item.Item;
 import item.ItemArmor;
 import item.ItemType;
 import item.ItemUsable;
@@ -55,10 +56,10 @@ public class Shop {
 					runBuyItem(ItemType.WEAPON);
 					break;
 				case MENU_BUY_ARMOR:
-					runBuyItem(ItemType.WEAPON);
+					runBuyItem(ItemType.ARMOR);
 					break;
 				case MENU_BUY_POTION:
-					runBuyItem(ItemType.WEAPON);
+					runBuyItem(ItemType.POTION);
 					break;
 				case MENU_GO_BACK:
 					return;
@@ -67,9 +68,44 @@ public class Shop {
 	}
 	
 	private void runBuyItem(String type) {
-		for (ItemWeapon weapon : itemWeapons) {
-			System.out.println(weapon);
+		List<Item> items = null;
+
+		switch (type) {
+			case ItemType.WEAPON:
+				items = (List)itemWeapons;
+				break;
+			case ItemType.ARMOR:
+				items = (List)itemArmors;
+				break;
+			case ItemType.POTION:
+				items = (List)itemUsables;
+				break;
+			default:
 		}
+		
+		if (items == null || items.size() == 0) {
+			System.out.println("구매할 아이템이 없습니다");
+			return;
+		}
+		
+		System.out.println("=== 리스트 ===");
+		for (int i = 0; i < items.size(); i++)
+			System.out.printf("%d. %s\n", i + 1, items.get(i));
+		System.out.println("============");
+		
+		int itemIndex = Input.getInputNumber("아이템 숫자");
+		
+		if (itemIndex < 0 || itemIndex >= items.size()) {
+			System.out.println("잘못된 아이템입니다");
+			return;
+		}
+		
+		Item item = items.get(itemIndex);
+		
+		if (item.PRICE > Player.getGuildMoney())
+			System.out.println("돈 부족으로 구매 불가");
+		
+		Player.addItemToGuild();
 	}
 
 	private void runSellItem() {
