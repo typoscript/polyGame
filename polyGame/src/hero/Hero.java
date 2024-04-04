958873f82a3346469b7fc638cd46c3e0f590b425
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 14:24:31 +0900

    IncreaseExpGain

--- polyGame/src/hero/Hero.java
+++ polyGame/src/hero/Hero.java
@@ -11,11 +11,16 @@ public abstract class Hero extends Unit {
 	protected int level = 1;
 	protected int exp = 0;
 	protected int expMax = 100;
+	protected int expExtraGainPercentage = 10;
 
 	protected boolean hasParty;
 	protected Weapon weapon;
 	protected Armor armor;
 	protected List<ItemUsable> usableItems;
+	
+	public void increaseExpGain() {
+		expExtraGainPercentage *= 1.1;
+	}
 	
 	abstract public void attack(Unit target);
 }
