598315906f1351d98198a87870125418b9857827
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 11:52:19 +0900

    Add Method: loadFile()

--- polyGame/src/main/FileManager.java
+++ polyGame/src/main/FileManager.java
@@ -5,7 +5,26 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class FileManager {
-	private FileReader fr;
-	private FileWriter fw;
-	private BufferedReader br;
+	private static FileWriter fw;
+	private static FileReader fr;
+	private static BufferedReader br;
+	
+	public static String loadFile(String fileName) {
+		String data = "";
+
+		try {
+			fr = new FileReader(fileName);
+			br = new BufferedReader(fr);
+			
+			while (br.ready())
+				data += br.readLine();
+			
+			br.close();
+			fr.close();
+		} catch (Exception e) {
+			System.out.println("파일 로드 실패");
+		}
+		
+		return data;
+	}
 }
