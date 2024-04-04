1dce5155580be6c442b0a79d3ab3a52429a4693f
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 11:42:40 +0900

    Refactor Code

--- polyGame/src/item/ItemWeapon.java
+++ polyGame/src/item/ItemWeapon.java
@@ -1,5 +1,10 @@
 package item;
 
-public interface ItemWeapon {
-	public void equip();
+public class ItemWeapon extends Item implements Weapon {
+	final int ATTACK_POWER;
+
+	public ItemWeapon(String name, int price, int attackPower) {
+		super(name, price);
+		this.ATTACK_POWER = attackPower;
+	}
 }
