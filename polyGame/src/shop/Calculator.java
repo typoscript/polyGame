4a65c978e690c8aa00fdbff7f62b9b84d14c3da4
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 16:45:04 +0900

    Add Method: getSellingTax()

--- polyGame/src/shop/Calculator.java
+++ polyGame/src/shop/Calculator.java
@@ -1,5 +1,10 @@
 package shop;
 
 public class Calculator {
+	private static int TAX_PERCENTAGE = 50;
 	private Calculator() { }
+	
+	public static int getSellingTax(int price) {
+		return (int)(price * (TAX_PERCENTAGE / 100.0));
+	}
 }
