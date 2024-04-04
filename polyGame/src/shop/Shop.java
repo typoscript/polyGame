5d4597c12bb96151632bcae1e0e401678cd21adb
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 11:55:10 +0900

    Add Method: printMenu()

--- polyGame/src/shop/Shop.java
+++ polyGame/src/shop/Shop.java
@@ -1,5 +1,14 @@
 package shop;
 
 public class Shop {
+	private final int MENU_BUY = 1;
+	private final int MENU_SELL = 2;
+	private final int MENU_QUIT = 0;
 
+	private void printMenu() {
+		System.out.println("=== 상점 ===");
+		System.out.println("1. 구매하기");
+		System.out.println("2. 판매하기");
+		System.out.println("0. 뒤로가기");
+	}
 }
