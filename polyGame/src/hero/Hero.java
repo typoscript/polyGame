4b5b8401d9723171bf1e63d98a1d865a4069bc89
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 18:07:57 +0900

    Add Methods: hasWeapon(), hasArmor()

--- polyGame/src/hero/Hero.java
+++ polyGame/src/hero/Hero.java
@@ -22,6 +22,14 @@ public abstract class Hero extends Unit {
 		expExtraGainPercentage *= 1.1;
 	}
 	
+	public boolean hasWeapon() {
+		return weapon != null;
+	}
+
+	public boolean hasArmor() {
+		return armor != null;
+	}
+	
 	public void equipItem(Weapon weapon) {
 		this.weapon = weapon;
 	}
@@ -30,12 +38,12 @@ public abstract class Hero extends Unit {
 		this.armor = armor;
 	}
 	
-	abstract public void attack(Unit target);
-	
 	public String getStatusOfWearables() {
 		String statusWeapon = weapon == null ? "없음" : "있음";
 		String statusArmor = armor == null ? "없음" : "있음";
 
 		return String.format("무기: %s | 방어구: %s\n", statusWeapon, statusArmor);
 	}
+
+	abstract public void attack(Unit target);
 }
