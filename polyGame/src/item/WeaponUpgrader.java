8934913ba6f41baf0b00a1e2ade7962280e108c9
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 10:38:58 +0900

    Add Unimplemented Method

--- polyGame/src/item/WeaponUpgrader.java
+++ polyGame/src/item/WeaponUpgrader.java
@@ -1,7 +1,14 @@
 package item;
 
+import main.Unit;
+
 public class WeaponUpgrader extends Item implements ItemUsable {
 	public WeaponUpgrader() {
 		super("무기 업그레이드", 3000);
+	}
+
+	@Override
+	public void use(Unit target) {
+		target.upgradeWeapon(10);
 	}
 }
