5f36b5e7c9d4f60fcafd874a5985e5fe4b242d99
typoscript <minkyu.kim.contact@gmail.com>
2024-04-04 10:15:20 +0900

    Add File: Item.java

---
+++ polyGame/src/item/Item.java
@@ -0,0 +1,11 @@
+package item;
+
+public class Item {
+	public final String NAME;
+	public final int PRICE;
+	
+	public Item(String name, int price) {
+		this.NAME = name;
+		this.PRICE = price;
+	}
+}
