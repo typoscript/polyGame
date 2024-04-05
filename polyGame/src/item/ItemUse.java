1606e4c53b7feeeea3f677d32eaf8933471ba7ed
typoscript <minkyu.kim.contact@gmail.com>
2024-04-05 17:25:49 +0900

    Rename Class: ItemUsable -> ItemUse

--- polyGame/src/item/ItemUse.java
+++ polyGame/src/item/ItemUse.java
@@ -2,10 +2,10 @@ package item;
 
 import main.Unit;
 
-public abstract class ItemUsable extends Item {
+public abstract class ItemUse extends Item {
 	final int STAT;
 
-	public ItemUsable(String name, int price, int stat) {
+	public ItemUse(String name, int price, int stat) {
 		super(name, price);
 		this.STAT = stat;
 	}
