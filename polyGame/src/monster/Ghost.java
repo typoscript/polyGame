86768c67dabb857c428fb609ff99fad1e4791fae
typoscript <minkyu.kim.contact@gmail.com>
2024-04-05 15:30:40 +0900

    Modify Method: silenceTarget()

--- polyGame/src/monster/Ghost.java
+++ polyGame/src/monster/Ghost.java
@@ -19,6 +19,6 @@ public class Ghost extends Unit {
 	public void silenceTarget(Unit target) {
 		target.setStatus(UnitStatus.SILENT);
 
-		System.out.printf("%s 다음턴 공격만 가능\n", target.getName());
+		System.out.printf("%s 침묵으로 다음턴 스킵\n", target.getName());
 	}
 }
