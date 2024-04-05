42426ec8ae6e088be5168e88277ba5a4e338fb9b
typoscript <minkyu.kim.contact@gmail.com>
2024-04-05 16:35:56 +0900

    Modify Method: chooseAction()

--- polyGame/src/hero/Warrior.java
+++ polyGame/src/hero/Warrior.java
@@ -1,6 +1,7 @@
 package hero;
 
 import utils.Input;
+import main.Player;
 import main.Unit;
 
 public class Warrior extends Hero implements Actionable {
@@ -22,6 +23,7 @@ public class Warrior extends Hero implements Actionable {
 	@Override
 	public void chooseAction(Unit target) {
 		final int ATTACK = 1;
+		final int ITEM_USE = 2;
 
 		if (isSilent()) {
 			handleSilence();
@@ -29,12 +31,14 @@ public class Warrior extends Hero implements Actionable {
 		}
 		
 		while (true) {
-			System.out.printf("[%s] 1) 공격\n", name);
+			System.out.printf("[%s] 1) 공격 2) 아이템 사용\n", name);
 			int action = Input.getInputNumber("메뉴");
 			
 			switch (action) {
 				case ATTACK:
 					attack(target);
+				case ITEM_USE:
+					Player.guild.inventory.useItem(this);
 					return;
 			}
 		}
