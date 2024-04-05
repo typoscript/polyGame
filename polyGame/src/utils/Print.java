d163148bb7339a0822b398e4bf8ef4c7a875aeea
typoscript <minkyu.kim.contact@gmail.com>
2024-04-05 12:17:12 +0900

    Change Package Name

--- polyGame/src/utils/Print.java
+++ polyGame/src/utils/Print.java
@@ -1,5 +1,12 @@
-package main;
+package utils;
+
+import java.util.List;
 
 public class Print {
-
+	public static void printListWithListNumber(List<?> list) {
+		System.out.println("=== 리스트 ===");
+		for (int i = 0; i < list.size(); i++)
+			System.out.printf("%d. %s\n", i + 1, list.get(i));
+		System.out.println("============");
+	}
 }
