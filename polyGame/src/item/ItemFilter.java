99275db6d2d71a7ecc791eb1d8bdc797f7fb32af
typoscript <minkyu.kim.contact@gmail.com>
2024-04-05 17:27:56 +0900

    Change Variable Type: Class<ItemUseable> -> Class<ItemUse>

--- polyGame/src/item/ItemFilter.java
+++ polyGame/src/item/ItemFilter.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ItemFilter {
 	public static final Class<ItemWeapon> WEAPON = ItemWeapon.class;
 	public static final Class<ItemArmor> ARMOR = ItemArmor.class;
-	public static final Class<ItemUsable> POTION = ItemUsable.class;
+	public static final Class<ItemUse> USEABLE = ItemUse.class;
 	
 	public static List<Item> filter(Class<?> type, List<Item> items) {
 		List<Item> filteredItems = new ArrayList<Item>();
