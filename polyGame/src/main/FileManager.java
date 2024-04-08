74b508437aa9991fc088f8749c8e7fcd9bd27053
typoscript <minkyu.kim.contact@gmail.com>
2024-04-08 14:24:36 +0900

    Add Method: saveHeroData()

--- polyGame/src/main/FileManager.java
+++ polyGame/src/main/FileManager.java
@@ -8,10 +8,45 @@ import java.util.List;
 import hero.Hero;
 
 public class FileManager {
-	private static String DATA_DIR = "src/data";
+	private static String DATA_DIR = "src/data/";
 	private static FileWriter fw;
 	private static FileReader fr;
 	private static BufferedReader br;
+	
+	public static void saveHeroData() {
+		final String HERO_FILE_NAME = "hero.txt";
+		String data = "";
+
+		List<Hero> members = Player.guild.getMemberAll();
+		
+		for (int i = 0; i < members.size(); i++) {
+			Hero hero = members.get(i);
+
+			data += hero.getName() + "/" +
+					hero.getLevel() + "/" +
+					hero.getHp() + "/" +
+					hero.getMaxHp() + "/" +
+					hero.getAttackPower() + "/" +
+					hero.getArmorPower() + "/" +
+					hero.getStatus() + "/" +
+					hero.getExp() + "/" +
+					hero.hasParty() + "/" +
+					hero.getWeapon().NAME + "/" +
+					hero.getArmor().NAME;
+			
+			if (i < members.size() - 1)
+				data += "\n";
+		}
+		
+		try {
+			fw = new FileWriter(DATA_DIR + HERO_FILE_NAME);
+			fw.write(data);
+			
+			fw.close();
+		} catch (Exception e) {
+			System.out.println("파일 저장 실패: " + HERO_FILE_NAME);
+		}
+	}
 	
 	public static String loadFile(String fileName) {
 		String data = "";
