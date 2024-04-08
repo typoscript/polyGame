3f5885664d151ab4639afedf41b53fb8b7f90031
typoscript <minkyu.kim.contact@gmail.com>
2024-04-08 16:00:49 +0900

    Modify Class

--- polyGame/src/item/Item.java
+++ polyGame/src/item/Item.java
@@ -1,11 +1,13 @@
 package item;
 
-public class Item {
+public abstract class Item {
 	public final String NAME;
 	public final int PRICE;
+	public final int STAT;
 	
-	public Item(String name, int price) {
+	public Item(String name, int price, int stat) {
 		this.NAME = name;
 		this.PRICE = price;
+		this.STAT = stat;
 	}
 }
