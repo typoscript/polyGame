785c365c33360622486fefd895d7682c0444dfdd
typoscript <minkyu.kim.contact@gmail.com>
2024-04-08 16:19:11 +0900

    Add Method: getType()

--- polyGame/src/item/ItemType.java
+++ polyGame/src/item/ItemType.java
@@ -4,4 +4,15 @@ public class ItemType {
 	public static final String WEAPON = "WEAPON";
 	public static final String ARMOR = "ARMOR";
 	public static final String HP_POTION = "HP_POTION";
+	
+	public static String getType(Item item) {
+		if (item instanceof ItemHpPotion)
+			return HP_POTION;
+		else if (item instanceof ItemWeapon)
+			return WEAPON;
+		else if (item instanceof ItemArmor)
+			return ARMOR;
+		
+		return null;
+	}
 }
