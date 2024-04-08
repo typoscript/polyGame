74ab3deb08fc1c08c2b1abfec4639f823d3add05
typoscript <minkyu.kim.contact@gmail.com>
2024-04-08 12:30:07 +0900

    Rename Variable: ARMOR -> ARMOR_POWER

--- polyGame/src/item/ItemArmor.java
+++ polyGame/src/item/ItemArmor.java
@@ -1,20 +1,20 @@
 package item;
 
 public class ItemArmor extends Item { 
-	public final int ARMOR;
+	public final int ARMOR_POWER;
 
 	public ItemArmor(String name, int price, int armor) {
 		super(name, price);
-		this.ARMOR = armor;
+		this.ARMOR_POWER = armor;
 	}
 
 	@Override
 	public ItemArmor clone() {
-		return new ItemArmor(NAME, PRICE, ARMOR);
+		return new ItemArmor(NAME, PRICE, ARMOR_POWER);
 	}
 
 	@Override
 	public String toString() {
-		return String.format("방어구: %s | 가격: %d | 방어력: %d", NAME, PRICE, ARMOR);
+		return String.format("방어구: %s | 가격: %d | 방어력: %d", NAME, PRICE, ARMOR_POWER);
 	}
 }
